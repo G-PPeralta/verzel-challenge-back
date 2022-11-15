a06c3f1f582f329f4e29a39b1122986e0c594f30
G-PPeralta <prates_peralta@hotmail.com>
2022-11-15 20:33:08 -0300

    feat: implements jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+const envSecret = process.env.JWT_SECRET_KEY;
+export const jwtConstants = {
+  secret: envSecret,
+};
