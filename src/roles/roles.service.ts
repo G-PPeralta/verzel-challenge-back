b89f25ef98749790bbb4b8f35a843b8f0bf7e492
G-PPeralta <prates_peralta@hotmail.com>
2022-11-15 20:20:39 -0300

    feat: create roles resource and implements create function

---
+++ src/roles/roles.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { prismaClient } from 'src/database/prismaClient';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
+
+@Injectable()
+export class RolesService {
+  async create(createRoleDto: CreateRoleDto) {
+    return await prismaClient.roles.create({ data: createRoleDto });
+  }
+
+  findAll() {
+    return `This action returns all roles`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} role`;
+  }
+
+  update(id: number, updateRoleDto: UpdateRoleDto) {
+    return `This action updates a #${id} role`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} role`;
+  }
+}
